c419a5d8846229aa567593b05d98b779960a4c6a
Luke Morse <luke.morse@gmail.com>
2020-07-02 10:49:29 -0500

    calculate district status when changing installation status

--- KW Install/Views/InstallationView/InstallationViewModel.swift
+++ KW Install/Views/InstallationView/InstallationViewModel.swift
@@ -20,5 +20,48 @@ class InstallationViewModel: ObservableObject {
     func setStatus(status: InstallationStatus) {
         self.installation.status = status
         Firestore.firestore().collection(Constants.kInstallationCollection).document(installation.installationID).setData(["status": status.rawValue], merge: true)
+        setDistrictStatus(installStatus: status)
+        
+    }
+    
+    func setDistrictStatus(installStatus: InstallationStatus) {
+        Firestore.firestore().collection(Constants.kDistrictCollection).document(installation.districtID).getDocument { (snapshot, error) in
+            if let error = error {
+                print(error)
+            } else {
+                let data = snapshot!.data()
+                if let districtStatus = data?["status"] as? Int {
+                    if installStatus == .complete && districtStatus == DistrictStatus.pending.rawValue {
+                        self.tryMarkDistrictComplete()
+                        //if district has been marked complete and install is not complete, adjust district status
+                    } else if installStatus != .complete && districtStatus == DistrictStatus.complete.rawValue {
+                        self.markDistrictPending()
+                    }
+                }
+            }
+        }
+    }
+    
+    func markDistrictPending() {
+        Firestore.firestore().collection(Constants.kDistrictCollection).document(self.installation.districtID).setData(["status": DistrictStatus.pending.rawValue], merge: true)
+    }
+    
+    func tryMarkDistrictComplete() {
+        //check if rest of installations in district are complete
+        Firestore.firestore().collection(Constants.kInstallationCollection).whereField("districtID", isEqualTo: self.installation.districtID).getDocuments { (snapshot, error) in
+            if let error = error {
+                print(error)
+            } else {
+                for document in snapshot!.documents {
+                    if let status = document.data()["status"] as? Int {
+                        if status != InstallationStatus.complete.rawValue {
+                            return
+                        }
+                    } else {return}
+                }
+                //all installations are complete
+                Firestore.firestore().collection(Constants.kDistrictCollection).document(self.installation.districtID).setData(["status": DistrictStatus.complete.rawValue], merge: true)
+            }
+        }
     }
 }
